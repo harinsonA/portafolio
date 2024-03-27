b38637242be39304f22c9c8bda5db97d801a8574
harinsonA <harrinson.arellan@gmail.com>
2024-03-26 23:12:01 -0300

    pesando que desarrollar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={fotoHarinson} alt="logo" />
         <p>
-          Amor mio comence con el proyecto, este proyecto se va ir actualizando
+          Viendo algun diseño el cual poder replicar
         </p>
       </header>
     </div>
