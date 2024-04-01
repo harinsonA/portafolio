50e1d9c7ece56acae017b81b00ebb0695b9e4a9b
harinsonA <harrinson.arellan@gmail.com>
2024-03-31 23:18:48 -0300

    feat: iniciar sección de experiencia laboral y actualizar estilos responsivos

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,13 +1,12 @@
 import '../styles/Curriculum.css'
 import React from "react";
-
-
+import ExperienciaLaboral from './ExperienciaLaboral';
 
 class Curriculum extends React.Component {
-
   render() {
     return (
       <section className="curriculum-container">
+        <ExperienciaLaboral />
       </section>        
     );
   }
---
+++ src/components/ExperienciaLaboral.jsx
@@ -0,0 +1,41 @@
+import '../styles/ExperienciaLaboral.css'
+import React from "react";
+
+
+class ExperienciaLaboral extends React.Component {
+  render() {
+    return (
+      <section className="experience-container">
+        <h2>EXPERIENCIA PROFESIONAL</h2>
+        <section className='card'>
+          <section className='company'>
+            <div className='company-name'>
+              <span>Rexmas S.A.</span>
+            </div>
+            <div className='time'>
+              <span>Junio 2024 - Presente</span>
+            </div>
+          </section>
+          <section className='range'>
+            <div className='Title'>
+              Desarrollador BackEnd Junior
+            </div>
+            <div className='Description'>
+              <ul>
+                <li>Implementar nuevas vistas y funcionalidades.</li>
+                <li>Realizar corrección de errores y solucionar problemas técnicos.</li>
+                <li>Refactorizar y optimizar el código existente para mejorar su rendimiento y mantenibilidad.</li>
+                <li>Desarrollar y documentar APIs para aplicaciones externas.</li>
+                <li>Crear y ejecutar pruebas unitarias y de integración.</li>
+                <li>Adaptar pantallas y diseños existentes a pantallas responsive.</li>
+                <li>Metodología ágil Scrum.</li>
+              </ul>
+            </div>
+          </section>
+        </section>
+      </section>
+    )
+  }
+}
+
+export default ExperienciaLaboral;
